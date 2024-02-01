84244d3ea0a245f777edc50b82a488b6209888a5
guigoliver <profguilherme.as@gmail.com>
2024-02-01 08:51:52 -0300

    feat: Product orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
 
 //Decorator
 @Module({
-  imports: [ProductsModule],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'nest',
+    entities: [Product],
+  }), ProductsModule],
   controllers: [AppController], // MVC
   providers: [AppService], //Negócio, camada de serviços
 })
